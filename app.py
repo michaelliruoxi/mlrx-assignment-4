3da2eb8dbc22d1ddf4a39d9b3da193a0f9a1d9d4
michaelliruoxi <michaelliruoxi@gmail.com>
2024-10-13 15:51:21 -0400

    implemented the search engine

--- app.py
+++ app.py
@@ -6,23 +6,54 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import nltk
 from nltk.corpus import stopwords
+import json
 
 nltk.download('stopwords')
 
 app = Flask(__name__)
 
+# Load dataset
+newsgroups = fetch_20newsgroups(subset='all')
+documents = newsgroups.data
+stop_words = list(set(stopwords.words('english')))
 
-# TODO: Fetch dataset, initialize vectorizer and LSA here
+# Create a Term-Document Matrix using TF-IDF
+vectorizer = TfidfVectorizer(stop_words=stop_words)
+X = vectorizer.fit_transform(documents)
 
+# Perform SVD (LSA)
+n_components = 100  # Number of dimensions to reduce to
+svd = TruncatedSVD(n_components=n_components)
+X_reduced = svd.fit_transform(X)
+
+# Inverse transform to approximate original space (optional if you need to visualize components)
+terms = vectorizer.get_feature_names_out()
+
+def preprocess_query(query):
+    """
+    Preprocess the user query (e.g., remove stop words, transform using vectorizer).
+    """
+    query_tfidf = vectorizer.transform([query])
+    query_reduced = svd.transform(query_tfidf)
+    return query_reduced
 
 def search_engine(query):
     """
-    Function to search for top 5 similar documents given a query
+    Function to search for top 5 similar documents given a query.
     Input: query (str)
     Output: documents (list), similarities (list), indices (list)
     """
-    # TODO: Implement search engine here
-    # return documents, similarities, indices 
+    query_reduced = preprocess_query(query)
+    # Compute cosine similarity between the query and all documents
+    similarities = cosine_similarity(query_reduced, X_reduced)
+    
+    # Get the top 5 most similar documents
+    top_n = 5
+    indices = np.argsort(similarities[0])[::-1][:top_n]
+    top_docs = [documents[i] for i in indices]
+    top_similarities = [similarities[0][i] for i in indices]
+    
+    return top_docs, top_similarities, indices.tolist()
 
 @app.route('/')
 def index():
@@ -32,7 +63,7 @@ def index():
 def search():
     query = request.form['query']
     documents, similarities, indices = search_engine(query)
-    return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices}) 
+    return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices})
 
 if __name__ == '__main__':
     app.run(debug=True)
