54c2f4626a3609554a9717d2d66c5b4c38b10586
michaelliruoxi <michaelliruoxi@gmail.com>
2024-10-13 15:52:09 -0400

    implemented display chart function

--- static/main.js
+++ static/main.js
@@ -33,10 +33,31 @@ function displayResults(data) {
 }
 
 function displayChart(data) {
-    // Input: data (object) - contains the following keys:
-    //        - documents (list) - list of documents
-    //        - indices (list) - list of indices   
-    //        - similarities (list) - list of similarities
-    // TODO: Implement function to display chart here
-    //       There is a canvas element in the HTML file with the id 'similarity-chart'
+    const similarityChart = document.getElementById('similarity-chart');
+
+    const trace = {
+        x: data.indices.map(i => `Doc ${i}`),
+        y: data.similarities,
+        type: 'bar',
+        text: data.similarities.map(sim => sim.toFixed(3)),
+        hovertext: data.indices.map((i, idx) => `Doc ${i}<br>Cosine Similarity: ${data.similarities[idx].toFixed(3)}`),
+        hoverinfo: 'text',  // This ensures that the hovertext shows
+        textposition: 'auto',
+        marker: {
+            color: 'rgba(55, 128, 191, 0.6)',
+            line: {
+                color: 'rgba(55, 128, 191, 1.0)',
+                width: 2
+            }
+        }
+    };
+
+    const layout = {
+        title: 'Cosine Similarity of Top Documents',
+        xaxis: { title: 'Document' },
+        yaxis: { title: 'Cosine Similarity', range: [0, 1] }
+    };
+
+    // Use Plotly to plot the data on the div
+    Plotly.newPlot(similarityChart, [trace], layout);
 }
